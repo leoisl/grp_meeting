1b3e4948a3a3f5d6e877034330b43c8b686ab3e5
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-12-07 11:46:46 +0000

    fix: formatting add.py and adding a better description for CLI

--- grp_meeting/add.py
+++ grp_meeting/add.py
@@ -1,12 +1,18 @@
 import argparse
-def add(a,b):
-    return a+b
 
-def main():
-    parser = argparse.ArgumentParser("Add two numbers")
-    parser.add_argument('a',type=int)
-    parser.add_argument('b',type=int)
+
+def add(a: int, b: int) -> int:
+    sum = a + b
+    return sum
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser("Add two integer numbers")
+    parser.add_argument("a", type=int)
+    parser.add_argument("b", type=int)
     args = parser.parse_args()
-    print(add(args.a,args.b))
-if __name__ == '__main__':
+    print(add(args.a, args.b))
+
+
+if __name__ == "__main__":
     main()
